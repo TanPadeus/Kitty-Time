106a48b0d68ea554cf3315d7df5b7dfea7e0e298
TanPadeus <62436809+TanPadeus@users.noreply.github.com>
2020-09-25 16:00:37 +0200

    Code cleaned (#3)

--- app/src/main/java/com/example/kittytime/models/Cat.kt
+++ app/src/main/java/com/example/kittytime/models/Cat.kt
@@ -3,20 +3,13 @@ package com.example.kittytime.models
 import com.squareup.moshi.Json
 
 
-data class Cat (
-//    @Json(name = "breeds") val breeds: Array<Breed>,
-//    @Json(name = "categories") val categories: Array<Category>,
-    @Json(name = "id") val id: String,
-    @Json(name = "url") val imageUrl: String,
-    @Json(name = "width") val width: Int,
-    @Json(name = "height") val height: Int
-) {
+data class Cat (@Json(name = "url") val url: String) {
     override fun equals(other: Any?): Boolean {
         if (other == null || javaClass != other.javaClass) return false
-        return id == (other as Cat).id
+        return url == (other as Cat).url
     }
 
     override fun hashCode(): Int {
-        return id.hashCode()
+        return url.hashCode()
     }
 }
